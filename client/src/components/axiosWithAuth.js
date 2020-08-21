7642c525e517e1f08865db4b850c25fbf147a580
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-21 11:09:28 -0400

    working on stage 1

---
+++ client/src/components/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const axiosWithAuth = () => {
+
+    const token = localStorage.getItem('token')
+
+    return axios.create({
+        basseURL: 'http://localhost:5000',
+        headers: {
+            Authorization: `${token}`
+        }
+    })
+}
